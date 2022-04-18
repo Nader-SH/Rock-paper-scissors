6c6c87304b3231913ff40eb0a6da44d385fe5cf8
Nader shak-shak <64221231+nadershakshak@users.noreply.github.com>
2022-04-19 00:35:48 +0300

    Add files via upload

---
+++ game.js
@@ -0,0 +1,67 @@
+var rock = document.getElementById("rock");
+var paper = document.getElementById("paper");
+var scissors = document.getElementById("scissors");
+var result = document.getElementById("result");
+
+let win = document.querySelector("#win");
+let draw = document.querySelector("#draw");
+let lose = document.querySelector("#lose");
+
+let winScore = 0;
+let drawScore = 0;
+let loseScore = 0;
+
+
+function ourComputerChoice() {
+    var randomObject = ["rock", "paper", "scissors"]
+    var myRandomNum = Math.floor(Math.random() * 3);
+
+    if (myRandomNum == 0) {
+        return randomObject[0];
+    } else if (myRandomNum == 1) {
+        return randomObject[1];
+    } else {
+        return randomObject[2];
+    }
+};
+
+var myChoice;
+
+function myOurChoice() {
+    var ourChoiceHtml = ourComputerChoice();
+
+    if ((myChoice == "rock" && ourChoiceHtml == "scissors") || (myChoice == "scissors" && ourChoiceHtml == "paper") || (myChoice == "paper" && ourChoiceHtml == "rock")) {
+        result.innerText = "Your choice = " + myChoice + " " + "==>" + " Computer Choice = " + ourChoiceHtml + "==> " + " You Win 😁 ";
+        winScore++;
+        win.textContent = winScore;
+
+    } else if (myChoice == ourChoiceHtml) {
+        result.innerText = "Try again " + "==>" + " Draw 🙄";
+        drawScore++;
+        draw.textContent = drawScore;
+
+    } else {
+        result.innerText = "Your choice = " + myChoice + " " + "==>" + " Computer Choice = " + ourChoiceHtml + "==> " + "You Loss 🙃";
+        loseScore++;
+        lose.textContent = loseScore;
+
+    }
+}
+
+function rockClick() {
+    myChoice = "rock";
+    myOurChoice();
+}
+
+function paperClick() {
+    myChoice = "paper"
+    myOurChoice();
+}
+
+function scissorsClick() {
+    myChoice = "scissors"
+    myOurChoice();
+}
+rock.onclick = rockClick;
+paper.onclick = paperClick;
+scissors.onclick = scissorsClick;
